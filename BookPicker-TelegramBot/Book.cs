c04542c749d55134b2c297ce6472d6f5e3386bf8
Alishan <MaksOff2@yandex.ru>
2024-09-29 12:13:47 +0300

    Создал класс Book

---
+++ BookPicker-TelegramBot/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPicker_TelegramBot
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int DateOfPublication { get; set; }
+        public string LinkToRead { get; set; }
+
+        public Book(string title, string author, int dateOfPublication, string linkToRead)
+        {
+            Title = title;
+            Author = author;
+            DateOfPublication = dateOfPublication;
+            LinkToRead = linkToRead;
+        }
+
+        public override string ToString()
+        {
+            return $"{Author}, {Title}, {DateOfPublication}";
+        }
+    }
+}
