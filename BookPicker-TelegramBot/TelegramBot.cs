17738cff996d3572d3d6973ab45694a3015dc40d
Alishan <MaksOff2@yandex.ru>
2024-09-29 12:07:58 +0300

    Сделал основу для бота

---
+++ BookPicker-TelegramBot/TelegramBot.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Diagnostics.Metrics;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+string token = "8142383621:AAGGxzdxmaj-gUtZ-pNtD6HMYR1YiJsNJ7I";
+TelegramBotClient telegramBotClient = new TelegramBotClient(
+    token: token
+    );
+var me = telegramBotClient.GetMeAsync().Result;
+telegramBotClient.StartReceiving(
+    updateHandler: UpdateHandler,
+    pollingErrorHandler: ErrorHandler
+    );
+Console.WriteLine($"Бот запущен : {me}");
+Console.ReadKey();
+
+async Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+{
+    Console.WriteLine(exception.Message);
+}
+
+async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+{
+    long chatId = update.Message.Chat.Id;
+    if (update.Message.Text != null)
+    {
+        if (update.Message.Text == "/start")
+        {
+            await client.SendTextMessageAsync(chatId, "");
+        }
+    }
+}
