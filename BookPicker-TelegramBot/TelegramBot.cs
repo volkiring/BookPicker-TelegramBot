09fe201636ce0edf8888e271498e1bde28a3601e
Alishan <MaksOff2@yandex.ru>
2024-09-30 18:15:58 +0300

    сделал главное меню

--- BookPicker-TelegramBot/TelegramBot.cs
+++ BookPicker-TelegramBot/TelegramBot.cs
@@ -1,7 +1,9 @@
 ﻿
+using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 string token = "8142383621:AAGGxzdxmaj-gUtZ-pNtD6HMYR1YiJsNJ7I";
 TelegramBotClient telegramBotClient = new TelegramBotClient(
@@ -27,7 +29,32 @@ async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationT
     {
         if (update.Message.Text == "/start")
         {
-            await client.SendTextMessageAsync(chatId, "");
+            await MainMenu(client, chatId);
         }
     }
+}
+
+async Task MainMenu(ITelegramBotClient client, long chatId)
+{
+    InlineKeyboardMarkup inlineKeyboard = new(new[]
+    {
+        new []
+        {
+            InlineKeyboardButton.WithCallbackData(text: "Книги месяца", callbackData: "booksOfMonth")
+        },
+        new []
+        {
+            InlineKeyboardButton.WithCallbackData(text: "Выбрать книгу", callbackData: "chooseBook")
+        },
+        new []
+        {
+            InlineKeyboardButton.WithCallbackData(text: "Закладки", callbackData: "bookmarks"),
+            InlineKeyboardButton.WithCallbackData(text: "Ежедневное чтение", callbackData: "everydayReading"),
+        }
+    });
+    await client.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Добро пожаловать! Выберите действие, которое хотите совершить:",
+        replyMarkup: inlineKeyboard
+        );
 }
